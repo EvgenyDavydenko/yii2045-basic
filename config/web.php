0cab4b134513f7d0556b0d0d0ae25a88e662959a
EvgenyDavydenko <frontend82@mail.ru>
2022-12-07 23:39:14 +0300

    friendly URL

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'UVSEqdZdpczzGi8HpO4SSK78f3M09tIo',
+            'baseUrl' =>'',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
